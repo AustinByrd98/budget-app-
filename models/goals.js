1be6c30f001dcd5bf65b9ffad147845baebca592
austinbyrd98 <allthingsnew98@gmail.com>
2023-11-27 19:35:17 -0500

    entries show page made but not tested; updated goals schema

--- models/goals.js
+++ models/goals.js
@@ -3,7 +3,10 @@ const mongoose = require("mongoose")
 const goalsSchema = new mongoose.Schema({
     name: {type:String, required: true},
     goalAmmount: {type: Number, required: true},
+    dateDue:Date,
     currentAmmount:{type: Number, required: true},
+    priority:{type:Number, required: true},
+    reward: String,
     disc: String,
     accomplished: Boolean
 })
